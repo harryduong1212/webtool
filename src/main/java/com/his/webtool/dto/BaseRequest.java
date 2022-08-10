b5cd677a321bd534d8330b7b68eba924641ca3d7
h-duong-cna <61999246+h-duong-cna@users.noreply.github.com>
2022-08-11 00:15:49 +0700

    do: add abstract class BaseRequest

---
+++ src/main/java/com/his/webtool/dto/BaseRequest.java
@@ -0,0 +1,23 @@
+package com.his.webtool.dto;
+
+import com.his.webtool.common.constant.RegexConst;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+public abstract class BaseRequest implements Serializable {
+
+  /** serialVersionUID */
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  @NotBlank
+  private String language;
+
+  @Pattern(regexp = RegexConst.UUID)
+  private String sessionId;
+}
